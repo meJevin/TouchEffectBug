18f0ed7d494ec766778fb1ac97dfa63c943cbd2d
Michael <mejevin@gmail.com>
2021-04-01 14:56:54 +0300

    initial commit

---
+++ TouchEffectBug/TouchEffectBug/Controls/TestTemplate.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TouchEffectBug.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestTemplate : ContentView
+    {
+        public static readonly BindableProperty HasTouchEffectsProperty =
+            BindableProperty.Create(nameof(HasTouchEffects), typeof(bool), typeof(TestTemplate), false,
+                propertyChanged: OnHasTouchEffectsChanged);
+
+        public bool HasTouchEffects
+        {
+            get { return (bool)GetValue(HasTouchEffectsProperty); }
+            set { SetValue(HasTouchEffectsProperty, value); }
+        }
+
+        static void OnHasTouchEffectsChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            TestTemplate i = (TestTemplate)bindable;
+
+            if ((bool)newValue == false)
+            {
+                i.RemoveEffects();
+            }
+            else
+            {
+                i.AddEffects();
+            }
+        }
+
+        public TestTemplate()
+        {
+            InitializeComponent();
+        }
+
+        public void RemoveEffects()
+        {
+            MainContainer.Effects.Remove(MainContainer.Effects[0]);
+            MainContainer.ClearValue(TouchEffect.PressedScaleProperty);
+            MainContainer.ClearValue(TouchEffect.AnimationEasingProperty);
+            MainContainer.ClearValue(TouchEffect.AnimationDurationProperty);
+            MainContainer.RemoveBinding(TouchEffect.LongPressCommandProperty);
+            MainContainer.RemoveBinding(TouchEffect.LongPressCommandParameterProperty);
+        }
+
+        public void AddEffects()
+        {
+            Binding longPressBinding = new Binding();
+            longPressBinding.Source = new RelativeBindingSource(RelativeBindingSourceMode.FindAncestorBindingContext,
+                typeof(MainPage), 1);
+            longPressBinding.Path = nameof(MainPage.LongPressCommand);
+
+            Binding longPressCommandBinding = new Binding();
+            longPressCommandBinding.Source = this.BindingContext;
+
+            MainContainer.SetValue(TouchEffect.PressedScaleProperty, 0.95);
+            MainContainer.SetValue(TouchEffect.AnimationEasingProperty, Easing.CubicOut);
+            MainContainer.SetValue(TouchEffect.AnimationDurationProperty, 650);
+            MainContainer.SetBinding(TouchEffect.LongPressCommandProperty, longPressBinding);
+            MainContainer.SetBinding(TouchEffect.LongPressCommandParameterProperty, longPressCommandBinding);
+        }
+    }
+}
---
+++ TouchEffectBug/TouchEffectBug/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.Runtime.CompilerServices;
+
+namespace TouchEffectBug
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        public ObservableCollection<string> Items { get; set; }
+        
+        public MainPage()
+        {
+            BindingContext = this;
+
+            InitializeComponent();
+
+            Items = new ObservableCollection<string>();
+            for (int i = 0; i < 500; ++i)
+            {
+                Items.Add("Test");
+            }
+            OnPropertyChanged(nameof(Items));
+        }
+
+
+        private ICommand longPressCommand;
+        public ICommand LongPressCommand
+        {
+            get
+            {
+                if (longPressCommand == null)
+                {
+                    longPressCommand = new Command(LongPress);
+                }
+
+                return longPressCommand;
+            }
+        }
+
+        private void LongPress()
+        {
+            Application.Current.MainPage.DisplayAlert("Long press", "Long press", "OK");
+        }
+
+        private bool hasTouchEffects;
+
+        public bool HasTouchEffects
+        {
+            get
+            {
+                return hasTouchEffects;
+            }
+
+            set
+            {
+                hasTouchEffects = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            HasTouchEffects = !HasTouchEffects;
+        }
+    }
+}
